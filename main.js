f234b296bc955177ec81181277b3100030cc4133
Li Zhang <bylizhang@gmail.com>
2018-02-05 23:42:41 -0800

    cards can be flipped and compared now

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 $(document).ready(initializeApp);
 
+var first_card_clicked = null;
+var second_card_clicked = null;
+
 function initializeApp() {
 	var card = $('#game .card');
-	$(card).on('click', flipCard);
+	$(card).on('click', '.back', hideBack);
 }
 
-function flipCard(event) {
-	var back = event.target;
-	console.log(back);
-	$(back[2]).toggleClass('hide');
+function hideBack() {
+	$(this).addClass('hide');
+	var frontImg = $(this).prev().children();
+	console.log(frontImg);
+	if (first_card_clicked === null) {
+		first_card_clicked = frontImg.attr('src');
+		console.log(first_card_clicked);
+	} else {
+		second_card_clicked = frontImg.attr('src');
+		console.log(second_card_clicked);
+		if (first_card_clicked === second_card_clicked) {
+			return
+		} else {
+			setTimeout(function(){
+				$('#game .card .back').removeClass('hide');
+			}, 2000);
+		}
+	}
+
+	
+}
+
+function getImageName() {
+	console.log(this);
 }
