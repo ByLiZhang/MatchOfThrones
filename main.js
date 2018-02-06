bd879ae9e7cecc0fe149495610bc9a784812d17b
Li Zhang <bylizhang@gmail.com>
2018-02-06 00:16:48 -0800

    need to fix card flipping logic

--- main.js
+++ main.js
@@ -11,20 +11,30 @@ function initializeApp() {
 function hideBack() {
 	$(this).addClass('hide');
 	var frontImg = $(this).prev().children();
+	var backImg = $(this);
+	var first_card_src = '';
+	var second_card_src = '';
 	console.log(frontImg);
 	if (first_card_clicked === null) {
-		first_card_clicked = frontImg.attr('src');
-		console.log(first_card_clicked);
+		first_card_clicked = frontImg;
+		first_card_src = first_card_clicked.attr('src');
+		console.log(first_card_src);
 	} else {
-		second_card_clicked = frontImg.attr('src');
-		console.log(second_card_clicked);
-		if (first_card_clicked === second_card_clicked) {
+		second_card_clicked = frontImg;
+		second_card_src = second_card_clicked.attr('src');
+		console.log(second_card_src);
+		if (first_card_src === second_card_src) {
 			return
 		} else {
+			console.log(second_card_clicked);
 			setTimeout(function(){
-				$('#game .card .back').removeClass('hide');
-			}, 2000);
+				// first_card_clicked.removeClass('hide');
+				// second_card_clicked.removeClass('hide');
+				$('#game .card .back').removeClass('hide'); // not good
+			}, 1000);
 		}
+		first_card_clicked = null;
+		second_card_clicked = null;
 	}
 
 	
