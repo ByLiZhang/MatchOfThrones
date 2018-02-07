a0b111758df202c51889d6bc3b506891230b5c12
Li Zhang <bylizhang@gmail.com>
2018-02-06 23:57:04 -0800

    basic functions are working, need stats

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $(document).ready(initializeApp);
 
 var first_card_clicked = null;
 var match_counter = 0;
+var attempt_counter = 0;
 
 function initializeApp() {
 	var card = $('#game .card');
@@ -9,12 +10,18 @@ function initializeApp() {
 }
 
 function handleClick() {
-	hideBack(this);
 	if (first_card_clicked === null) {
 		first_card_clicked = this;
+		// console.log('1st clicked', first_card_clicked);
+		hideBack(this);
 	} else {
-		var second_card_clicked = this;
-		compareCards(first_card_clicked, second_card_clicked);
+		if(!$(this).hasClass('hide')){
+			var second_card_clicked = this;
+			// console.log('2nd clicked',second_card_clicked);
+			hideBack(this);
+			attempt_counter++;
+			compareCards(first_card_clicked, second_card_clicked);
+		}	
 	}
 }
 
@@ -22,18 +29,35 @@ function hideBack(card) {
 	$(card).addClass('hide');
 }
 
+function showBack(card) {
+	$(card).removeClass('hide');
+}
+
 function compareCards(card1, card2) {
 	var cardName1 = $(card1).find('.front img').attr('src');
 	var cardName2 = $(card2).find('.front img').attr('src');
+	// console.log(cardName1);
+	// console.log(cardName2);
 	if (cardName1 === cardName2){
 		match_counter++;
-		updateScore(match_counter);
+		// console.log(match_counter);
+		updateStats(match_counter);
 	} else {
-		$(card1).removeClass('hide');
-		$(card2).removeClass('hide');
+		setTimeout(function(){
+			showBack(card1);
+			showBack(card2);
+		}, 1500);
 	}
+	first_card_clicked = null;  //no need to check if second_card_clicked = null;
 }
 
-function updateScore(argument) {
-	// body...
+function updateStats(score) {
+	if (score%9 === 0 && score !== 0){
+		$('#game .stats .games-played .value').text((score/9).toString());
+		updateAttempts(attempt_counter);
+	} 
+}
+
+function updateAttempts(num) {
+	$('#game .stats .attempts .value').text((num/2).toString());
 }
